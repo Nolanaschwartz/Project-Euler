80f8cabb6e0a0c83b82e0e3cff58887d27619e07
Nolan <nolan.schwartz@student.csulb.edu>
2020-01-30 20:16:05 -0800

    Problem 4

---
+++ Problem 4/Problem 4.ts
@@ -0,0 +1,29 @@
+function largestPalindromeNumber(): Array<number> | undefined {
+    let product = 0;
+    let multiplierOne = 0;
+    let multiplierTwo = 0;
+
+    for (let x = 999; x > 0; x--) {
+        for (let y = 999; y > 0; y--) {
+            if (isPalindrome(x * y) && (x * y > product)) {
+                product = x * y;
+                multiplierOne = x;
+                multiplierTwo = y;
+            }
+        }
+    }
+    return [multiplierOne, multiplierTwo]
+}
+
+function isPalindrome(value: number): boolean {
+    const numberString = value.toString();
+    for (let i = 0; i < numberString.length; i++) {
+        if (numberString[i] !== numberString[numberString.length - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+const [x, y] = largestPalindromeNumber();
+console.log(x + ' * ' + y + ' = ' + x * y);
