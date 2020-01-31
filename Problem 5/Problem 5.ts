0b0050b13d13eee50790898da8274cb218406033
Nolan <nolan.schwartz@student.csulb.edu>
2020-01-30 20:43:21 -0800

    Problem 5 + add problem to 4

---
+++ Problem 5/Problem 5.ts
@@ -0,0 +1,34 @@
+// 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+//
+// What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+function smallestMultiple(): number {
+    let product = 20;
+
+    while (isDivisible(product)) {
+        product += 20;
+    }
+
+    return product;
+}
+
+function isDivisible(value: number): boolean {
+    const multiples = [];
+
+    for (let i = 1; i <= 20; i++) {
+        multiples.push(i)
+    }
+
+    multiples.forEach(multiple => {
+        if (value % multiple !== 0) {
+            return false;
+        }
+    });
+
+    return true;
+}
+
+
+console.log(smallestMultiple());
+
+
